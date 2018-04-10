96474d0c8560ddd91e483fa34fead1338cac0a13
Leticia de Souza <leticia.souza212@gmail.com>
2018-04-10 19:43:47 -0300

    Improve getGuessedWord function

--- hang.py
+++ hang.py
@@ -32,16 +32,24 @@ def isWordGuessed(secretWord, lettersGuessed):
     return True
 
 
-def getGuessedWord():
-
+def getGuessedWord(secretWord, lettersGuessed):
     guessed = ''
+    for letter in secretWord:
+        if letter in lettersGuessed:
+            guessed += letter
+        else:
+            guessed += '_ '
 
     return guessed
 
 
-def getAvailableLetters():
+def getAvailableLetters(lettersGuessed):
     # 'abcdefghijklmnopqrstuvwxyz'
     available = string.ascii_lowercase
+
+    for letter in available:
+        if letter in lettersGuessed:
+            available = available.replace(letter, '')
 
     return available
 
@@ -57,39 +65,26 @@ def hangman(secretWord):
     while not isWordGuessed(secretWord, lettersGuessed) and guesses > 0:
         print('You have ', guesses, 'guesses left.')
 
-        available = getAvailableLetters()
-        for letter in available:
-            if letter in lettersGuessed:
-                available = available.replace(letter, '')
-
+        available = getAvailableLetters(lettersGuessed)
         print('Available letters', available)
+
         letter = input('Please guess a letter: ')
         if letter in lettersGuessed:
 
-            guessed = getGuessedWord()
-            for letter in secretWord:
-                if letter in lettersGuessed:
-                    guessed += letter
-                else:
-                    guessed += '_ '
+            guessed = getGuessedWord(secretWord, lettersGuessed)
 
             print('Oops! You have already guessed that letter: ', guessed)
         elif letter in secretWord:
             lettersGuessed.append(letter)
 
-            guessed = getGuessedWord()
-            for letter in secretWord:
-                if letter in lettersGuessed:
-                    guessed += letter
-                else:
-                    guessed += '_ '
+            guessed = getGuessedWord(secretWord, lettersGuessed)
 
             print('Good Guess: ', guessed)
         else:
             guesses -= 1
             lettersGuessed.append(letter)
 
-            guessed = getGuessedWord()
+            guessed = getGuessedWord(secretWord, lettersGuessed)
             for letter in secretWord:
                 if letter in lettersGuessed:
                     guessed += letter
